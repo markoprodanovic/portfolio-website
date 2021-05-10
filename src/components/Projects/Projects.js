f47d5cd67d08ec8f55ef45d9bd40682a732e4b4b
Marko Prodanovic <marko.prodanovic@outlook.com>
2021-05-09 18:16:48 -0700

    Fixed resume

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,40 +1,39 @@
-import React, {useState, useEffect} from 'react'
-import styles from './Projects.module.css'
-import ProjectCard from './ProjectCard/ProjectCard'
+import React, { useState, useEffect } from "react";
+import styles from "./Projects.module.css";
+import ProjectCard from "./ProjectCard/ProjectCard";
 
-import data from './projects.json'
+import data from "./projects.json";
 const Projects = () => {
+    const [projects, setProjects] = useState([]);
 
-  const [projects, setProjects] = useState([])
+    useEffect(() => {
+        setProjects(
+            data.map((project) => {
+                return (
+                    <ProjectCard
+                        projectTitle={project.title}
+                        projectDescription={project.description}
+                        projectImgAddress={project.imgAddress}
+                        gitHubLink={project.githubLInk}
+                        demoLink={project.demoLink}
+                        stack={project.stack}
+                    />
+                );
+            })
+        );
+    }, []);
 
-  useEffect(() => {
-    setProjects(data.map((project) => {
-      return (
-        <ProjectCard
-          projectTitle = {project.title}
-          projectDescription= {project.description}
-          projectImgAddress={project.imgAddress}
-          gitHubLink={project.githubLInk}
-          demoLink={project.demoLink}
-          stack={project.stack}
-        />
-      ) 
-    }))
-  }, [])
-
-  return (
-    <div>
-      <div id="mywork" className={styles.anchorMargin}/>
-      <div className={styles.container}>
-        <div className={styles.title}>
-          <h2>MY WORK</h2>
+    return (
+        <div>
+            <div id="mywork" className={styles.anchorMargin} />
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    <h2>MY WORK</h2>
+                </div>
+                <div className={styles.projects}>{projects}</div>
+            </div>
         </div>
-        <div className={styles.projects}>
-          {projects}
-        </div>
-    </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Projects
+export default Projects;
